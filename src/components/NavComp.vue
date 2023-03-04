bdcb096fbb73e8a282cf0ae45586960caa084b46
horodeski <geovana.horodeski06@gmail.com>
2023-03-03 23:45:04 -0300

    gsh

---
+++ src/components/NavComp.vue
@@ -0,0 +1,38 @@
+<script>
+export default {}
+</script>
+<template>
+  <nav>
+    <div class="coisas-da-loja">
+      <ul>
+        <li><a href="">SAC</a></li>
+        <li><a href="">TRABALHE CONOSCO</a></li>
+        <li><a href="">DISQUE DENÚNCIA</a></li>
+        <li><a href="">LOJAS</a></li>
+      </ul>
+    </div>
+    <div class="nav-principal">
+      <div class="logo">
+        <h1>P10</h1>
+      </div>
+      <div class="pesquisa">
+        <input type="text">
+        <div class="lupa">
+          <box-icon name='search-alt'></box-icon>
+        </div>
+      </div>
+      <div class="user-carrinho">
+        <div>
+          <box-icon size='lg' name='cart-alt'></box-icon>
+        </div>
+        <div class="user">
+          <img src="@/assets/img/eduardo.jpeg" alt="">
+          <h3>edudasilva</h3>
+        </div>
+      </div>
+    </div>
+    <div class="navegacao">
+
+    </div>
+  </nav>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: /\.vue$/,
+  loader: 'vue-loader',
+  options: {
+    loaders: {
+      scss: 'vue-style-loader!css-loader!sass-loader', // <style lang="scss">
+      sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax' // <style lang="sass">
+    },
+  },
   plugins: [vue()],
   resolve: {
     alias: {
