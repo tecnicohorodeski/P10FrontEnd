744e389609ce1c82f341fd19668dfa69d6a234be
horodeski <geovana.horodeski06@gmail.com>
2023-03-04 15:03:16 -0300

    gsh

--- src/components/NavComp.vue
+++ src/components/NavComp.vue
@@ -16,23 +16,41 @@ export default {}
         <h1>P10</h1>
       </div>
       <div class="pesquisa">
-        <input type="text">
+        <input type="text" />
         <div class="lupa">
-          <box-icon name='search-alt'></box-icon>
+          <box-icon name="search-alt"></box-icon>
         </div>
       </div>
       <div class="user-carrinho">
         <div>
-          <box-icon size='lg' name='cart-alt'></box-icon>
+          <box-icon size="lg" name="cart-alt"></box-icon>
         </div>
         <div class="user">
-          <img src="@/assets/img/eduardo.jpeg" alt="">
+          <img src="@/assets/img/eduardo.jpeg" alt="" />
           <h3>edudasilva</h3>
         </div>
       </div>
     </div>
     <div class="navegacao">
-
+      <div class="dropdown">
+        <button class="dropbtn">TODAS AS CATEGORIAS</button>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+      <ul>
+        <li>
+          <a href="">DESCONTOS</a>
+        </li>
+        <li>
+          <a href="">MERCADO</a>
+        </li>
+        <li>
+          <a href="">LIVRARIA</a>
+        </li>
+      </ul>
     </div>
   </nav>
 </template>
